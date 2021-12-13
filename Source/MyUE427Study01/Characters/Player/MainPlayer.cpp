8116c1e9d42d246a5b169d0c8dab7b57c9e36eb3
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-12-14 00:12:24 +0800

    add

--- Source/MyUE427Study01/Characters/Player/MainPlayer.cpp
+++ Source/MyUE427Study01/Characters/Player/MainPlayer.cpp
@@ -3,40 +3,39 @@
 #include "MainPlayer.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 AMainPlayer::AMainPlayer()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	
+
 	springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	springArm->SetupAttachment(GetRootComponent());
 	springArm->TargetArmLength = 600.0f;
 
 	followCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	followCamera->SetupAttachment(springArm, USpringArmComponent::SocketName);
+
+	GetCapsuleComponent()->SetCapsuleSize(35.0f, 100.0f);
 }
 
 // Called when the game starts or when spawned
 void AMainPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AMainPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void AMainPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
