91932e1e7956d18624c2e2e54ad6cf1fda3457ec
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-12-15 00:46:37 +0800

    add

---
+++ Source/MyUE427Study01/GameLogic/Level1GameMode.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyUE427Study01/GameLogic/Level1GameMode.h"
+
---
+++ Source/MyUE427Study01/GameLogic/Level1GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyUE427Study01/MyUE427Study01GameModeBase.h"
+#include "Level1GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY01_API ALevel1GameMode : public AMyUE427Study01GameModeBase
+{
+	GENERATED_BODY()
+	
+};
