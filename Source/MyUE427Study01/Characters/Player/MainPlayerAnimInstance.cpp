8b9b83681d3b56bf70ab4378f37850420db08234
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-12-28 00:10:01 +0800

    add

--- Source/MyUE427Study01/Characters/Player/MainPlayerAnimInstance.cpp
+++ Source/MyUE427Study01/Characters/Player/MainPlayerAnimInstance.cpp
@@ -3,11 +3,28 @@
 
 #include "MainPlayerAnimInstance.h"
 
+#include "MainPlayer.h"
+#include "GameFramework/PawnMovementComponent.h"
+
 void UMainPlayerAnimInstance::NativeInitializeAnimation()
 {
-	
+	playerRef = Cast<AMainPlayer>(TryGetPawnOwner());
 }
 
 void UMainPlayerAnimInstance::UpdateAnimationProperties()
 {
+	if (playerRef == nullptr)
+	{
+		playerRef = Cast<AMainPlayer>(TryGetPawnOwner());
+	}
+
+	if(playerRef)
+	{
+		FVector speedVec = playerRef->GetVelocity();
+		FVector planeSpeed = FVector(speedVec.X, speedVec.Y, 0.0f);
+
+		speed = planeSpeed.Size();
+
+		bIsInAir = playerRef->GetMovementComponent()->IsFalling();
+	}
 }
