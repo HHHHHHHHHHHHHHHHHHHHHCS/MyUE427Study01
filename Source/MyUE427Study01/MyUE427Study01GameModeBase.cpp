1cf49d76646e750c72f2860881501aebc216c8d2
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-01-14 00:10:37 +0800

    add

--- Source/MyUE427Study01/MyUE427Study01GameModeBase.cpp
+++ Source/MyUE427Study01/MyUE427Study01GameModeBase.cpp
@@ -3,3 +3,19 @@
 
 #include "MyUE427Study01GameModeBase.h"
 
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+void AMyUE427Study01GameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (MainUIClass)
+	{
+		MainUI = CreateWidget<UUserWidget>(UGameplayStatics::GetPlayerController(this, 0), MainUIClass);
+		if(MainUI)
+		{
+			MainUI->AddToViewport();
+		}
+	}
+}
--- Source/MyUE427Study01/MyUE427Study01GameModeBase.h
+++ Source/MyUE427Study01/MyUE427Study01GameModeBase.h
@@ -13,5 +13,14 @@ UCLASS()
 class MYUE427STUDY01_API AMyUE427Study01GameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="UI Widgets")
+	TSubclassOf<class UUserWidget> MainUIClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="UI Widgets")
+	UUserWidget* MainUI;
+
+protected:
+	virtual void BeginPlay() override;
 };
