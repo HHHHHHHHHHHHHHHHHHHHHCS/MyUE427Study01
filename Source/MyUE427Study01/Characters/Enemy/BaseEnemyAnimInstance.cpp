831521dc48aeb1514ddd056a863675c4d066b71b
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-02-11 00:23:26 +0800

    add

---
+++ Source/MyUE427Study01/Characters/Enemy/BaseEnemyAnimInstance.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseEnemyAnimInstance.h"
+
+void UBaseEnemyAnimInstance::NativeInitializeAnimation()
+{
+	baseEnemyRef = Cast<ABaseEnemy>(TryGetPawnOwner());
+}
+
+void UBaseEnemyAnimInstance::UpdateAnimationProperties()
+{
+	if (baseEnemyRef == nullptr)
+	{
+		baseEnemyRef = Cast<ABaseEnemy>(TryGetPawnOwner());
+	}
+
+	if(baseEnemyRef)
+	{
+		const FVector speedVec = baseEnemyRef->GetVelocity();
+		const FVector planeSpeed = FVector(speedVec.X, speedVec.Y, 0.0f);
+
+		speed = planeSpeed.Size();
+	}
+}
---
+++ Source/MyUE427Study01/Characters/Enemy/BaseEnemyAnimInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemy.h"
+#include "Animation/AnimInstance.h"
+#include "BaseEnemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY01_API UBaseEnemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Animation Properties")
+	float speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Animation Properties")
+	ABaseEnemy* baseEnemyRef;
+
+public :
+	virtual void NativeInitializeAnimation() override;
+
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties();
+};
